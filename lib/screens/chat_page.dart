70f084fba500343e7f185e5d622eb2fee3c92424
hassanmouusahassan <hassanmoussa70181281@gmail.com>
2024-01-04 23:49:21 +0200

    here i have done the homeclub with the bottom navigation bar

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const Chatpage({super.key});
+  const ChatPage({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
