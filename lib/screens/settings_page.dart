97634345470d417ba290fbba5047f5fd5f850edc
Omar Abbas <covmar90@gmail.com>
2024-01-05 21:22:10 +0200

    dark theme done

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -75,7 +75,7 @@ class _SettingsPageState extends State<SettingsPage> {
         onUpdate: updateProfile,
       ),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(1.0, 0.0);
+        var begin = const Offset(1.0, 0.0);
         var end = Offset.zero;
         var curve = Curves.ease;
 
